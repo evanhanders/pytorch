4da29ca0aa9a065d48549856f1896358ea182e23
fduwjj <fduwjj@gmail.com>
2023-11-15 14:45:55 -0800

    [NCCL PG] Enable storing nccl traces into storage and make it configurable
    ghstack-source-id: 3105f6cc421f99f6c0fdfaa74efd8506d7fd3be7
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113503

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.hpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.hpp
@@ -313,6 +313,16 @@ class TORCH_API ProcessGroupNCCL : public Backend {
     friend class ProcessGroupNCCL;
   };
 
+  class DebugInfoWriter {
+   public:
+    DebugInfoWriter(int rank);
+    virtual ~DebugInfoWriter();
+    virtual void write(const std::string& ncclTrace);
+
+   protected:
+    std::string filename_;
+  };
+
   struct Options : Backend::Options {
     // NOTE: timeout in ProcessGroupNCCL::Options denote the timeout for
     // operations. This is only used when blockingWait_ is enabled.
@@ -505,6 +515,10 @@ class TORCH_API ProcessGroupNCCL : public Backend {
 
   void enableCollectivesTiming() override;
 
+  // Provide an API for users to define their own ways to store NCCL debug info.
+  void registerDebugInfoWriter(
+      std::unique_ptr<ProcessGroupNCCL::DebugInfoWriter> writer);
+
   // Tests if the UCC fallback path is available
   bool isUCCAvailable() const;
 
@@ -634,6 +648,11 @@ class TORCH_API ProcessGroupNCCL : public Backend {
   void workCleanupLoop();
 
   void runHookLoop();
+
+  // In the timeout case and we will dump debug info such as the NCCL flight
+  // recorder to storage. Down the road, if we have more complicated or blocking
+  // operations, we might need to use a side thread to do it.
+  void dumpDebuggingInfo();
 
   // Desync debug helper
   void logWorkStart(WorkNCCL& work);
@@ -855,6 +874,9 @@ class TORCH_API ProcessGroupNCCL : public Backend {
   uint64_t seq_{0};
 
   std::exception_ptr watchDogException_ = nullptr;
+
+  // The callback function to store NCCL debug info.
+  std::unique_ptr<ProcessGroupNCCL::DebugInfoWriter> debugInfoWriter_ = nullptr;
 
 #ifdef USE_NCCL_WITH_UCC
   // ProcessGroupUCC shared library handle and ProcessGroup pointer
